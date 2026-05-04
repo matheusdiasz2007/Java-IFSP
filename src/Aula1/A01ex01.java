b199eca25d6202fc33ba700a7481956ab085a65d
matheusdiasz2007 <matheus108532@gmail.com>
2026-05-04 16:36:13 -0300

    add

---
+++ src/Aula1/A01ex01.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Aula1;
+
+/**
+ *
+ * @author mathe
+ */
+public class A01ex01 {
+        
+    /**
+     *
+     * @param args
+     */
+    public static void main(string[] args) {
+        System.out.println("Matheus Dias Pereira");
+}
+
+    
+}
